30d006ffa93f46e806216c9ad3ed54db47918805
Joao Vitor <vitorgrandinabreu@gmail.com>
2021-05-13 18:31:16 -0300

    domain model, database seed

--- backend/src/main/java/com/uniamerica/restaurante/entity/Seller.java
+++ backend/src/main/java/com/uniamerica/restaurante/entity/Seller.java
@@ -1,2 +1,48 @@
-package com.uniamerica.restaurante.entity;public class seller {
+package com.uniamerica.restaurante.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_sellers")
+
+public class Seller {
+
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    private String name;
+
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> sales = new ArrayList<>();
+
+    public Seller(){}
+
+    public Seller(long id, String name) {
+        Id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Sale> getSales() {
+        return sales;
+    }
+
+
 }
